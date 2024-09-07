9451a4c49f0a88d449bc5dc8aee840f8e001e812
Nikita Agarwal <nikitaagr313@gmail.com>
2024-09-07 20:03:14 +0530

    Created first test case

--- src/app/string-calculator/string-calculator.service.spec.ts
+++ src/app/string-calculator/string-calculator.service.spec.ts
@@ -13,4 +13,11 @@ describe('StringCalculatorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return 0 for empty string', () => {
+
+    //this will return error as add function is not created
+    expect(service.add("")).toBe(0);
+  });
+  
 });
