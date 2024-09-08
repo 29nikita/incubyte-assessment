8ea330378a0a3ae492db3bf78a32665f2a468304
Nikita Agarwal <nikitaagr313@gmail.com>
2024-09-08 10:32:27 +0530

    code to handle multiple delimiters of single char

--- src/app/string-calculator/string-calculator.service.ts
+++ src/app/string-calculator/string-calculator.service.ts
@@ -10,41 +10,48 @@ export class StringCalculatorService {
   delimiterEndIndex: any;
   delimiterString: string = "";
 
-  add(numbers: string): number{
+  add(numbers: string): number {
     this.count++;
 
-    if(!numbers){
+    if (!numbers) {
       return 0;
     }
 
     let delimiter = /\n|,/;
 
-    if(numbers.startsWith("//")){
+    if (numbers.startsWith("//")) {
       this.delimiterEndIndex = numbers.indexOf("\n");
-      if(numbers[2] === "["){
-        this.delimiterString = numbers.substring(3, this.delimiterEndIndex-1);
+
+      const delimiterSection = numbers.substring(2, this.delimiterEndIndex);
+      const delimiterMatch = delimiterSection.match(/\[.*?\]/g);
+
+      if (delimiterMatch) {
+        const delimiterPatterns = delimiterMatch.map((del) => del.slice(1, -1).replace(/[.*+?^${}()|[\]\\]/g, "\\$&"));
+        delimiter = new RegExp(delimiterPatterns.join("|"), "g");
+      }else if (numbers[2] === "[") {
+        this.delimiterString = numbers.substring(3, this.delimiterEndIndex - 1);
         delimiter = new RegExp(this.delimiterString.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"));
-      }else{
+      } else {
         this.delimiterString = numbers[2];
         delimiter = new RegExp(this.delimiterString);
       }
 
-      numbers = numbers.substring(this.delimiterEndIndex+1);
+      numbers = numbers.substring(this.delimiterEndIndex + 1);
     }
 
     this.numArray = numbers.split(delimiter).map((num) => parseInt(num, 10));
     this.numArray = this.numArray.filter(num => num <= 1000);
-    
+
     const negatives = this.numArray.filter(num => num < 0);
 
-    if(negatives.length > 0){
+    if (negatives.length > 0) {
       throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
     }
 
     return this.numArray.reduce((sum, num) => sum + num, 0);
   }
 
-  getCountOfAddFunction(): number{
+  getCountOfAddFunction(): number {
     return this.count;
   }
 }
