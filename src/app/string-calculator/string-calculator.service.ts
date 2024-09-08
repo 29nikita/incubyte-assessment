c07679ca85729d0035ff7129c07ededf2e315902
Nikita Agarwal <nikitaagr313@gmail.com>
2024-09-08 10:52:12 +0530

    refactoring code for checking negatives

--- src/app/string-calculator/string-calculator.service.ts
+++ src/app/string-calculator/string-calculator.service.ts
@@ -38,13 +38,17 @@ export class StringCalculatorService {
     this.numArray = numbers.split(delimiter).map((num) => parseInt(num, 10));
     this.numArray = this.numArray.filter(num => num <= 1000);
 
-    const negatives = this.numArray.filter(num => num < 0);
+    this.checkForNegatives(this.numArray);
+
+    return this.numArray.reduce((sum, num) => sum + num, 0);
+  }
+
+  checkForNegatives(numArray: number[]){
+    const negatives = numArray.filter(num => num < 0);
 
     if (negatives.length > 0) {
       throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
     }
-
-    return this.numArray.reduce((sum, num) => sum + num, 0);
   }
 
   getCountOfAddFunction(): number {
